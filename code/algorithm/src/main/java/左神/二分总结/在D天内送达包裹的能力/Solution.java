ef0a852ab2efbf672c27ba694010c61ba4242edb
luoxinpeng <21444455@life.hkbu.edu.hk>
2022-04-16 22:13:45 +0800

    日常更新

---
+++ code/algorithm/src/main/java/左神/二分总结/在D天内送达包裹的能力/Solution.java
@@ -0,0 +1,36 @@
+package 左神.二分总结.在D天内送达包裹的能力;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int weight : weights) {
+            max = Math.max(max,weight);
+        }
+        int left = max;
+        int right = max * weights.length;
+        int res = -1;
+        Arrays.sort(weights);
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int temp = 1;
+            int sum = 0;
+            for (int weight : weights) {
+                if(sum + weight >middle){
+                    sum = 0;
+                    temp++;
+                }
+                sum += weight;
+            }
+            if(temp <= days) {
+                res = middle;
+                right = middle - 1;
+            }
+            else
+                left = middle + 1;
+        }
+        return res;
+    }
+}
