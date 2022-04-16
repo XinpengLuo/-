20134ca6814d7906d589cba499d6ec77f182d1ae
luoxinpeng <21444455@life.hkbu.edu.hk>
2022-04-17 00:04:11 +0800

    update

---
+++ code/algorithm/src/main/java/左神/二分总结/适龄的朋友/Solution.java
@@ -0,0 +1,31 @@
+package 左神.二分总结.适龄的朋友;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int numFriendRequests(int[] ages) {
+        Arrays.sort(ages);
+        int sum = 0;
+        int left = 0;
+        // 0.5x + 7 < y <= x
+        int right = 0;
+        for(int i = 0; i < ages.length; i++) {
+            if(ages[i] < 15)
+                continue;
+            left = i;
+            while(left >= 0 && ages[left] > 0.5 * ages[i] + 7)
+                left--;
+            right = i;
+            while(right < ages.length && ages[right] == ages[i])
+                right++;
+            sum += (right - 1 - (left + 1));
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {20,30,100,110,120};
+        System.out.println(s.numFriendRequests(arr));
+    }
+}
