d9722025bb76ede84e51beb85edb8395e0d03a7e
luoxinpeng <21444455@life.hkbu.edu.hk>
2022-05-07 18:34:11 +0800

    最长上升子序列 二分版

---
+++ code/面试真题/阿里子集/Main.java
@@ -0,0 +1,19 @@
+package 面试真题.阿里子集;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+//    public static int getAnswer(int[][] arr){
+//        Arrays.sort(arr, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] t1, int[] t2) {
+//                return t1[0] - t2[0];
+//            }
+//        });
+//        int[] dp = new int[arr.length];
+//        dp[0] = 1;
+//        for(int i = 1;)
+//    }
+
+}
