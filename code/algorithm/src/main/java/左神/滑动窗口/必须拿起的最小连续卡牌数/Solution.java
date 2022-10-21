46a47fb03597a55b793e6a47214206cbc0cda0b8
luoxinpeng <21444455@life.hkbu.edu.hk>
2022-10-21 11:10:23 +0800

    great

---
+++ code/algorithm/src/main/java/左神/滑动窗口/必须拿起的最小连续卡牌数/Solution.java
@@ -0,0 +1,17 @@
+package 左神.滑动窗口.必须拿起的最小连续卡牌数;
+
+import java.util.*;
+
+class Solution {
+    public int minimumCardPickup(int[] cards) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int res = -1;
+        for(int i = 0; i < cards.length; i++){
+            if(map.containsKey(cards[i])) {
+                res = res == -1 ? i - map.get(cards[i]) + 1 : Math.min(res, i - map.get(cards[i]) + 1);
+            }
+            map.put(cards[i],i);
+        }
+        return res;
+    }
+}
---
+++ code/algorithm/src/main/java/面试真题/神策现场/Solution.java
@@ -0,0 +1,2 @@
+package 面试真题.神策现场;public class Solution {
+}
